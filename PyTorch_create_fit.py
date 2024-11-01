52f998518f4f018c0fb71f48fdfb701adf25fb73
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2024-11-01 13:53:14 +0500

    PyTorch_create_fit.py

--- PyTorch_create_fit.py
+++ PyTorch_create_fit.py
@@ -4,42 +4,56 @@ import torchvision.transforms as transforms
 
 class NN(nn.Module):
     def __init__(self, inst_norm=False):
-        super(Gen,self).__init__()
-        self.n1 = conv(dim_c, dim_g, 4, 2, 1) 
-        self.n2 = conv_n(dim_g, dim_g*2, 4, 2, 1, inst_norm=inst_norm)
-        self.n3 = conv_n(dim_g*2, dim_g*4, 4, 2, 1, inst_norm=inst_norm)
-        self.n4 = conv_n(dim_g*4, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.n5 = conv_n(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.n6 = conv_n(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.n7 = conv_n(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.n8 = conv(dim_g*8, dim_g*8, 4, 2, 1)
-        self.m1 = tconv_n(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.m2 = tconv_n(dim_g*8*2, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.m3 = tconv_n(dim_g*8*2, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.m4 = tconv_n(dim_g*8*2, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
-        self.m5 = tconv_n(dim_g*8*2, dim_g*4, 4, 2, 1, inst_norm=inst_norm)
-        self.m6 = tconv_n(dim_g*4*2, dim_g*2, 4, 2, 1, inst_norm=inst_norm)
-        self.m7 = tconv_n(dim_g*2*2, dim_g*1, 4, 2, 1, inst_norm=inst_norm)
-        self.m8 = tconv(dim_g*1*2, dim_c, 4, 2, 1)
-        self.tanh = nn.Tanh()
+        super(NN,self).__init__()
+        self.n1 = nn.Conv2d(dim_c, dim_g, 4, 2, 1) 
+        self.n2 = nn.Conv2d(dim_g, dim_g*2, 4, 2, 1, inst_norm=inst_norm)
+        self.n3 = nn.Conv2d(dim_g*2, dim_g*4, 4, 2, 1, inst_norm=inst_norm)
+        self.n4 = nn.Conv2d(dim_g*4, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.n5 = nn.Conv2d(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.n6 = nn.Conv2d(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.n7 = nn.Conv2d(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.n8 = nn.Conv2d(dim_g*8, dim_g*8, 4, 2, 1)
+        self.m1 = nn.ConvTranspose2d(dim_g*8, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.m2 = nn.ConvTranspose2d(dim_g*8*2, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.m3 = nn.ConvTranspose2d(dim_g*8*2, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.m4 = nn.ConvTranspose2d(dim_g*8*2, dim_g*8, 4, 2, 1, inst_norm=inst_norm)
+        self.m5 = nn.ConvTranspose2d(dim_g*8*2, dim_g*4, 4, 2, 1, inst_norm=inst_norm)
+        self.m6 = nn.ConvTranspose2d(dim_g*4*2, dim_g*2, 4, 2, 1, inst_norm=inst_norm)
+        self.m7 = nn.ConvTranspose2d(dim_g*2*2, dim_g*1, 4, 2, 1, inst_norm=inst_norm)
+        self.m8 = nn.ConvTranspose2d(dim_g*1*2, dim_c, 4, 2, 1)
     def forward(self,x):
         n1 = self.n1(x)
-        n2 = self.n2(F.leaky_relu(n1, 0.2))
-        n3 = self.n3(F.leaky_relu(n2, 0.2))
-        n4 = self.n4(F.leaky_relu(n3, 0.2))
-        n5 = self.n5(F.leaky_relu(n4, 0.2))
-        n6 = self.n6(F.leaky_relu(n5, 0.2))
-        n7 = self.n7(F.leaky_relu(n6, 0.2))
-        n8 = self.n8(F.leaky_relu(n7, 0.2))
-        m1 = torch.cat([F.dropout(self.m1(F.relu(n8)), 0.5, training=True), n7], 1)
-        m2 = torch.cat([F.dropout(self.m2(F.relu(m1)), 0.5, training=True), n6], 1)
-        m3 = torch.cat([F.dropout(self.m3(F.relu(m2)), 0.5, training=True), n5], 1)
-        m4 = torch.cat([self.m4(F.relu(m3)), n4], 1)
-        m5 = torch.cat([self.m5(F.relu(m4)), n3], 1)
-        m6 = torch.cat([self.m6(F.relu(m5)), n2], 1)
-        m7 = torch.cat([self.m7(F.relu(m6)), n1], 1)
-        m8 = self.m8(F.relu(m7))
-        return self.tanh(m8)
+        n1 = nn.ReLU(n1)
+        n2 = self.n2(n1)
+        n2 = nn.ReLU(n2)
+        n3 = self.n3(n2)
+        n3 = nn.ReLU(n3)
+        n4 = self.n4(n3)
+        n4 = nn.ReLU(n4)
+        n5 = self.n5(n4)
+        n5 = nn.ReLU(n5)
+        n6 = self.n6(n5)
+        n6 = nn.ReLU(n6)
+        n7 = self.n7(n6)
+        n7 = nn.ReLU(n7)
+        n8 = self.n8(n7)
+        n8 = nn.ReLU(n8)
+        m1 = torch.cat([n8, n7], 1)
+        m1 = nn.ReLU(m1)
+        m2 = torch.cat([m1, n6], 1)
+        m2 = nn.ReLU(m2)
+        m3 = torch.cat([m2, n5], 1)
+        m3 = nn.ReLU(m3)
+        m4 = torch.cat([m3, n4], 1)
+        m4 = nn.ReLU(m4)
+        m5 = torch.cat([m4, n3], 1)
+        m5 = nn.ReLU(m5)
+        m6 = torch.cat([m5, n2], 1)
+        m6 = nn.ReLU(m6)
+        m7 = torch.cat([m6, n1], 1)
+        m7 = nn.ReLU(m7)
+        m8 = self.m8(m7)
+        return nn.Tanh(m8)
 
 list_of_transformations = [
     transforms.Resize((256,256)),
@@ -49,11 +63,11 @@ transform = transforms.Compose(list_of_transformations)
 
 #dataset
 #L__test
-#¦   L__ Cat
-#¦   L__ Dog
+#В¦   L__ Cat
+#В¦   L__ Dog
 #L__train
-#¦   L__ Cat
-#¦   L__ Dog
+#В¦   L__ Cat
+#В¦   L__ Dog
 train_dataset = ImageFolder(root="/dataset/train/", transform=transform)
 val_dataset = ImageFolder(root="/dataset/val/", transform=transform)
 batch_size = 50
@@ -62,7 +76,7 @@ load_Train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
              shuffle=True, num_workers=num_workers)
 load_Test = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, 
             shuffle = False, num_workers=num_workers)
-#картинка - текст
+#ГЄГ Г°ГІГЁГ­ГЄГ  - ГІГҐГЄГ±ГІ
 
 
 
@@ -88,9 +102,9 @@ dataset_path = "new_data"
 train_path = os.path.join(dataset_path, "train")
 valid_path = os.path.join(dataset_path, "test")
 
-train_x = sorted(glob(os.path.join(train_path, "image", "*png")))#возвращает 2 массива путей
+train_x = sorted(glob(os.path.join(train_path, "image", "*png")))#ГўГ®Г§ГўГ°Г Г№Г ГҐГІ 2 Г¬Г Г±Г±ГЁГўГ  ГЇГіГІГҐГ©
 train_y = sorted(glob(os.path.join(train_path, "mask", "*png")))
-train_x, train_y = shuffle(train_x, train_y, random_state=42) # перемешивание
+train_x, train_y = shuffle(train_x, train_y, random_state=42) # ГЇГҐГ°ГҐГ¬ГҐГёГЁГўГ Г­ГЁГҐ
 image_pairs = np.hstack((train_x, train_y))
 
 train_dataset = PairedImageDataset(path="/dataset/train/", transform=transform, image_pairs)
@@ -100,7 +114,7 @@ load_Train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
              shuffle=True, num_workers=num_workers)
 load_Test = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, 
             shuffle = False, num_workers=num_workers)
-#картинка-картинка
+#ГЄГ Г°ГІГЁГ­ГЄГ -ГЄГ Г°ГІГЁГ­ГЄГ 
 
 
 NN = NN(False)
