b8751c09b883560c04b139272434f47a90af6525
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2024-11-03 20:51:30 +0500

    NumPy_Transformer.py

--- NumPy_Transformer.py
+++ NumPy_Transformer.py
@@ -8,7 +8,7 @@ def attention(x, WQ, WK, WV):
     Q = x @ WQ
 
     scores = Q @ K.T 
-    scores = scores / np.sqrt(d_key)  # we just changed this
+    scores = scores / np.sqrt(d_key)
     scores = softmax(scores)
     scores = scores @ V
     return scores
@@ -60,17 +60,11 @@ global vocabulary = [
 ]
 W1 = np.random.randn(4, 10)
 W2 = np.random.randn(10, 4)
+W3 = np.random.randn(10, 10) # длина ввода * длина словаря
 b1 = np.random.randn(10)
 b2 = np.random.randn(4)
-W_attent = np.random.randn(4, 6) 
-b_attent = np.random.randn(6)
-# матрицы + матрицы внимания
-
-inn = input()
-inn = inn.lower
-input = inn.split(" ")
-embedding = pos_embedd(input, np.zeros(10))# вход
-
+b3 = np.random.randn(10) # длина словаря
+# матрицы 
 
 WK1 = np.random.randn(3, 10) # размерность головы х размер запроса
 WV1 = np.random.randn(3, 10)
@@ -80,17 +74,22 @@ WK2 = np.random.randn(3, 10)
 WV2 = np.random.randn(3, 10)
 WQ2 = np.random.randn(3, 10)
 
-W_linear = np.random.randn(10, 10) # длина ввода * длина словаря
-b_linear = np.random.randn(10) # длина словаря
+W_attent = np.random.randn(4, 6) 
+b_attent = np.random.randn(6)
+# матрицы внимания
+
+inn = input()
+inn = inn.lower
+input = inn.split(" ")
+embedding = pos_embedd(input, np.zeros(10))# вход
 
 # Начинается кодер.
 
 attention1 = attention(embedding, WQ1, WK1, WV1)
-
 attention2 = attention(embedding, WQ2, WK2, WV2)
-
 attentions = np.concatenate([attention1, attention2], axis=1)
-Z = attentions @ W_attent
+
+Z = forward_prop(attentions, W_attent, b_attent)
 Z = layer_norm(Z + embedding)
 
 output = forward_prop(Z, W1, b1)
@@ -102,18 +101,16 @@ output = layer_norm(output + Z)
 # Начинается декодер. Здесь max_iters = 5 for i in range(max_iters):
 
 attention1 = attention(output, embedding, WQ1, WK1, WV1)
-
 attention2 = attention(output, embedding, WQ2, WK2, WV2)
-
 attentions = np.concatenate([attention1, attention2], axis=1)
+
 Z = forward_prop(attentions, W_attent, b_attent)
 Z = layer_norm(Z_decoder + [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]) #здесь символ SOS
 
 attention1 = decoder_attention(output, Z, WQ1, WK1, WV1)
-
 attention2 = decoder_attention(output, Z, WQ2, WK2, WV2)
-
 attentions = np.concatenate([attention1, attention2], axis=1)
+
 Z_decoder = forward_prop(attentions, W_attent, b_attent)
 Z_decoder = layer_norm(Z_decoder + Z)
 
@@ -124,10 +121,10 @@ output = layer_norm(output + Z_decoder)
 
 # Здесь добавить обучение
 
-logits = forward_prop(output[-1], W_linear, b_linear)# Используем для предсказания только последние выходные данные
-probs = softmax([logits])
+logits = forward_prop(output[-1], W3, b3)# Используем для предсказания только последние выходные данные
+logits = softmax([logits])
 
 out = []
-out += vocabulary[np.argmax(probs)]
+out += vocabulary[np.argmax(logits)]
 print(out)
 input()
