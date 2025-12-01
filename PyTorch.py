f0a121d7b9e9d4a6d5323e94212a44840be79f0d
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-12-01 21:38:12 +0500

    Update PyTorch.py

--- PyTorch.py
+++ PyTorch.py
@@ -1,6 +1,6 @@
-# Определение нейронной сети
 import torch
 import torch.nn as nn
+from torchsummary import summary
 import numpy as np
 import random
 import math
@@ -45,82 +45,92 @@ class Attention(nn.Module):
        return n4
 
 class NN(nn.Module):
-   def __init__(self, dim):
+   def __init__(self, dim, max_context):
        super(NN, self).__init__()
        self.dim = dim
        self.a_dim = dim // 2
        self.max_context=max_context
        self.embeddings = nn.Embedding(self.dim, self.a_dim)
-       self.att = nn.MultiheadAttention(self.dim, 1)
-       # self.a_linear = nn.Linear(self.a_dim, self.dim)
-       self.linear1 = nn.Linear(self.dim, self.dim//2)
-       self.linear2 = nn.Linear(self.dim//2, self.dim)
-       self.linear3 = nn.Linear(self.dim, self.dim)
+       self.att = nn.MultiheadAttention(self.a_dim, 1)
+       self.linear1 = nn.Linear(self.a_dim, self.a_dim)
+       self.linear2 = nn.Linear(self.a_dim, self.a_dim)
+       self.linear3 = nn.Linear(self.a_dim, self.dim)
+       self.layer_norm1 = nn.LayerNorm(self.a_dim)
+       self.layer_norm2 = nn.LayerNorm(self.a_dim)
+       self.layer_norm3 = nn.LayerNorm(self.a_dim)
+       self.layer_norm4 = nn.LayerNorm(self.a_dim)
+       self.layer_norm5 = nn.LayerNorm(self.a_dim)
+       self.sigmoid = nn.Sigmoid()
 
    def forward(self, x):
+
     current_sequence = x.clone()#ИСПРАВЛЕНИЕ!
-    outputs =  torch.empty(dim)
-    for i in range(max_context//2-1):#!!!ОСНОВНОЙ ПРИКОЛ ТРАНСФОРМЕРА - ЦИКЛ
+    outputs_list =  []
+
+    for i in range(self.max_context):#!!!ОСНОВНОЙ ПРИКОЛ ТРАНСФОРМЕРА - ЦИКЛ
       #ENCODER
       y=self.embeddings(current_sequence)
       a,b = self.att(y,y,y)
       a = a + y
-      b1 = nn.LayerNorm(self.dim)(a)
+      b1 = self.layer_norm1(a)
 
       #BOTTLENECK
       m = self.linear1(b1)
       m = self.linear2(m)
       m = m + b1
-      encod = nn.LayerNorm(self.dim)(m)
+      encod = self.layer_norm2(m)
 
       #DECODER1
       a,b = self.att(encod,encod,encod) # Декодер=Энкодер+Энкодер с инпутом старого
       decoder1 = a + encod
-      decoder1 = nn.LayerNorm(self.dim)(decoder1)
+      decoder1 = self.layer_norm3(decoder1)
 
       #DECODER2
       a,b = self.att(decoder1,decoder1,encod)
       decoder2 = a + decoder1
-      decoder2 = nn.LayerNorm(self.dim)(decoder2)
+      decoder2 = self.layer_norm4(decoder2)
 
       #BOTTLENECK
       b = self.linear1(decoder2)
       b = self.linear2(b)
       b = b + decoder2
-      b = nn.LayerNorm(self.dim)(b)
+      b = self.layer_norm5(b)
 
       output = self.linear3(b)
-      output = nn.Sigmoid()(output)
+      output =self.sigmoid(output)
 
-      outputs = torch.vstack((outputs, output[0][0]))
+      outputs_list.append(output[0])
 
-      next_token = torch.argmax(output[0][0], dim=-1)
-      addr=(current_sequence == 0).nonzero(as_tuple=True)[-1][0]#адрес первого нуля
-      current_sequence[0][addr] = next_token.unsqueeze(0).unsqueeze(0)
+      next_token = torch.argmax(output[0], dim=-1)
+      zero_position=(current_sequence == 0).nonzero(as_tuple=True)[0]#адрес первого нуля
+      if len(zero_position) > 0:
+        current_sequence[zero_position[0]] = next_token
       #выделить токен и прибавить к тексту
 
       if next_token==vocabulary.index("EOS"): break
-      if current_sequence[0][-1]!=0.: break
+      if current_sequence[-1]!=0: break
 
-    return current_sequence,outputs;
+    return current_sequence, outputs_list;
 
 
-def to_pretensor(s):
+def to_pretensor(s): # Получение int
   not_tensor = []
   inputt=s.split(" ")
   for word in inputt:
-    if len(not_tensor)==max_context: return not_tensor
-    try:not_tensor.append(vocabulary.index(word))
+    if len(not_tensor)==max_context: return not_tensor # Обрезка ввода до max_context
+    try:not_tensor.append(vocabulary.index(word)) # Обработка неизвестных слов
     except: print (word+" not in list")
   return not_tensor
 
 
-def to_tensor(not_tensor):
+def to_tensor(not_tensor): # Получение int64
 
-  embedding=np.array(not_tensor, dtype=np.int64)#int64/float32
-  tensor = torch.from_numpy(embedding)
-  example=torch.zeros(max_context)
+  np_tensor=np.array(not_tensor, dtype=np.int64)#int64
+  tensor = torch.from_numpy(np_tensor)
+
+  example=torch.zeros(max_context) # Заполнение нулями
   tensor=nn.utils.rnn.pad_sequence([tensor,example]).T[0]
+
   return tensor
 
 
@@ -145,7 +155,6 @@ def traindata():
   return vocabulary, clean_data
 
 
-
 vocabulary = [
     "N",
     "hello",
@@ -159,51 +168,56 @@ vocabulary = [
     "c",
 ]
 
-
-reward = 0
-loss_c = nn.CrossEntropyLoss()
+vocabulary, clean_data = traindata()
+dim = len(vocabulary)
+max_context = 10
 epochs = 10
 
 
 
+model = NN(dim, max_context)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
 
-#ОСНОВНОЕ ОБУЧЕНИЕ
-vocabulary, clean_data = traindata()
+# ОСНОВНОЕ ОБУЧЕНИЕ
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+# loss_c = nn.CrossEntropyLoss()
 
-dim = len(vocabulary)
-max_context = 10
-model = NN(dim)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
-
-dataset = CustomDataset(clean_data)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=5)
+# dataset = CustomDataset(clean_data)
+# dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=5)
 
 
-for i, (x, y) in enumerate(dataloader):#ОБУЧЕНИЕ
-  output,outputs=model(x)
-  y_probs=to_probs(y)
-  print('ВЫВОД GPT')
-  for word in output[0]: print(f'{vocabulary[word]} {word}')
-  loss = loss_c(outputs, y_probs)
-  optimizer.zero_grad()
-  loss.backward()
-  optimizer.step()
-  if i % 100 ==0:
-    print('Train Epoch: [{}/{}], Loss {:.4f}'.format
-   (i+1, len(dataloader), loss))
-      input()
+# for i, (x, y) in enumerate(dataloader):#ОБУЧЕНИЕ
+#   print(x)
+#   output,outputs=model(x)
+#   y_probs=to_probs(y)
+#   print('Output')
+#   for word in output[0]: print(f'{vocabulary[word]} {word}')
+#   loss = loss_c(outputs, y_probs)
+#   optimizer.zero_grad()
+#   loss.backward()
+#   optimizer.step()
+#   if i % 100 ==0:
+#     print('Train Epoch: [{}/{}], Loss {:.4f}'.format
+#    (i+1, len(dataloader), loss))
+#     input()
+
+
+
+
 
 
 # ВВОД
 for iteration in range(epochs):
- s = input()
- pretensor = to_pretensor(s)
- tensor=to_tensor(pretensor)
- print(f'original {tensor}')
- input()
- output = model(tensor)
- print (f"ITERATION {iteration}: ")
- print(f'input {tensor}')
- print(f'output {output}')
- print ("----")
+    s = input()
+    pretensor = to_pretensor(s)
+    tensor=to_tensor(pretensor)
+    print (f"ITERATION {iteration}: ")
+    print(f'Input {tensor}')
+
+
+
+    output, outputs = model(tensor)
+    print(f'Output {output}')
+    print ("----")
